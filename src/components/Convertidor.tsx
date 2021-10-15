6edf791cf846a602ae48c71a749a16267d7ff2de
Davila99 <davilaeliseo453@gmail.com>
2021-10-14 20:56:45 -0600

    Actualizaciones en el componente  Convertidor

--- src/components/Convertidor.tsx
+++ src/components/Convertidor.tsx
@@ -4,67 +4,69 @@ import React,{useState} from 'react'
 import { View, Text, TextInput, StyleSheet, Button, Alert } from 'react-native'
 
 export const Convertidor = () => {
-    const [origin, setOrigin] = useState('cm')
-
-    const handleChangeOrigin = (texto:string) =>{
-        setOrigin(texto)
-    }
-
-    const [destino,setDestino] =useState('km')
-    
-    const handleChangeDestino =(texto:string) =>{
-        setDestino(texto)
-    }
-
-    const [convertir,setConvertir] =useState('')
+    const [origin, setOrigin] = useState('')
+    const [destino,setDestino] =useState('')
+    const [convertir,setConvertir] =useState(0)
 
     const handleChangeConvertir=(texto:string) =>{
-        setConvertir(texto)
+     const numero = parseFloat(texto)
+        setConvertir(numero)
     }
 
-    const [resultado,setResultado] =useState('')
+    const [resultado,setResultado] =useState(0)
 
-    const handleChangeResultado=(texto:string)=>{
-
+    const handleCalcular=()=>{
+        let result:number
+        if (origin ==="cm" && destino ==="km") {
+            result = convertir /100;
+            setResultado(result)
+        }else{
+            if (origin ==="km" && destino ==="cm") {
+                result = convertir *100000;
+                setResultado(result)
+            }
+        }
+        
+        
     }
+ 
 
     return (
         <View>
-            <Text style={styles.texto}> Origen </Text>
+            <Text style={styles.texto}>Unidad de  Origen </Text>
             <TextInput 
             style={styles.input}
             defaultValue={origin}
             maxLength={2}
-            onChangeText={(text)=> handleChangeOrigin(text)}
+            onChangeText={setOrigin}
 
              />
-            <Text style={styles.texto}> Destino </Text>
+            <Text style={styles.texto}> Unidad de Destino </Text>
             <TextInput 
             style={styles.input} 
             defaultValue={destino}
             maxLength={2}
-            onChangeText={(text)=> handleChangeDestino(text)}
+            onChangeText={setDestino}
 
             />
-            <Text style={styles.texto}> Convertir </Text>
+            <Text style={styles.texto}>A Convertir </Text>
 
             <TextInput
              style={styles.input}
-             defaultValue={convertir}  
+             defaultValue={convertir.toString()}  
              maxLength={10} 
              onChangeText={(text)=> handleChangeConvertir(text)}
              />
 
-            <Text style={styles.texto}> Resultado </Text>
+            <Text style={styles.texto}> Resultado de convertir {origin} a {destino} </Text>
             <TextInput
              style={styles.input}
-             defaultValue={resultado}
+             defaultValue={resultado.toString()}
              maxLength={10}
-             onChangeText={(text)=>handleChangeResultado(text)}
               />
             <View style={styles.button}>
                 <Button title="Calcular"
-                    onPress={() => alert('Registrado')}
+                    onPress={handleCalcular}
                 />
 
             </View>
