6ba10eaa53131f35661cfed08ffd57054687d6b7
Davila99 <davilaeliseo453@gmail.com>
2021-10-14 19:07:44 -0600

    Hemos agregado el componente de Convertidor

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Convertidor } from './src/components/Convertidor';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Eliseo Antonio davila Ojeda</Text>
+      <Convertidor/>
       <StatusBar style="auto" />
     </View>
   );
